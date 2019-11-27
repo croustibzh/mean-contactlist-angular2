54ca03bc49408265129714ff1c0bef8e9e3da191
croustizbh <m.ledot@outlook.com>
2019-11-26 21:42:12 -0500

    Added import for ngForm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ContactDetailsComponent } from './contacts/contact-details/contact-details.component';
 import { ContactListComponent } from './contacts/contact-list/contact-list.component';
@@ -12,7 +12,8 @@ import { ContactListComponent } from './contacts/contact-list/contact-list.compo
     ContactListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
